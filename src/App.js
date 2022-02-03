1bf59fdba27eb58c2b914234365f1e1302aeab67
Jun <junli666@gmail.com>
2022-02-03 22:28:01 +1100

    state is object

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
 import "./App.css";
 import React, { useState } from "react";
 
-function return4() {
-  console.log("called");
-  return 4;
-}
 function App() {
-  const [count, setCount] = useState(return4); //called Once pass function is good idea
-  //const [count, setCount] = useState(return4()); //called each time
+  const [{ count, theme }, setState] = useState(() => ({
+    count: 0,
+    theme: "blue",
+  })); //called Once pass function is good idea
+
   const addCount = () => {
-    setCount(count + 1);
+    setState((prevState) => ({ theme, count: prevState.count + 1 })); // direct return in ()
   };
-  const addCount2WrongWay = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-  };
-  const addCount2RightWay = () => {
-    setCount((PrevCount) => PrevCount + 1);
-    setCount((PrevCount) => PrevCount + 1);
-  };
+
   return (
     <>
       <button
         onClick={() => {
-          setCount(count - 1);
+          setState((prevState) => {
+            return { theme, count: prevState.count - 1 };
+          });
         }}
       >
         -
       </button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={addCount}>+</button>
-      <button onClick={addCount2WrongWay}>++wrong</button>
-      <button onClick={addCount2RightWay}>++right</button>
     </>
   );
 }
